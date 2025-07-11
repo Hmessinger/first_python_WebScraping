0acf39886f4281ddd33eaaa0a21f33da3d82a61b
Hogan Messinger <messinger8771@gmail.com>
2025-07-10 22:23:20 -0500

    Updated our printing statement

--- web_scrape.py
+++ web_scrape.py
@@ -6,7 +6,5 @@ soup = BeautifulSoup(page_to_scrape.text, "lxml")
 quotes = soup.find_all("span", attrs={"class":"text"})
 authors = soup.find_all("small", attrs={"class":"author"})
 
-for quote in quotes:
-    print(quote.text)
-for author in authors:
-    print(author.text)
+for quote, author in zip(quotes, authors):
+    print(quote.text + " - By: " + author.text)
