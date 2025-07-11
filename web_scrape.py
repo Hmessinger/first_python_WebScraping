f6b178f4504d69a7958d90e7670b887b6af2501b
Hogan Messinger <messinger8771@gmail.com>
2025-07-10 22:42:54 -0500

    Wrote our scraped data to a csv file

--- web_scrape.py
+++ web_scrape.py
@@ -1,10 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 page_to_scrape = requests.get("http://quotes.toscrape.com")
 soup = BeautifulSoup(page_to_scrape.text, "lxml")
 quotes = soup.find_all("span", attrs={"class":"text"})
 authors = soup.find_all("small", attrs={"class":"author"})
 
+file = open("scraped_quotes.csv", "w", encoding='utf-8')
+writer = csv.writer(file)
+
+writer.writerow(["QUOTES", "AUTHORS"])
+
 for quote, author in zip(quotes, authors):
     print(quote.text + " - By: " + author.text)
+    writer.writerow([quote.text, author.text])
+
+file.close()
