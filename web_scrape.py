967d7826123bdfa5f8d2d7600b04da20f52192f5
Hogan Messinger <messinger8771@gmail.com>
2025-07-10 22:16:58 -0500

    First web scraper

---
+++ web_scrape.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+page_to_scrape = requests.get("http://quotes.toscrape.com")
+soup = BeautifulSoup(page_to_scrape.text, "lxml")
+quotes = soup.find_all("span", attrs={"class":"text"})
+authors = soup.find_all("small", attrs={"class":"author"})
+
+for quote in quotes:
+    print(quote.text)
+for author in authors:
+    print(author.text)
